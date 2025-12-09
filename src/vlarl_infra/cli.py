0d5a0c1aee584d6719f7e7f10edf0aee20c06038
Chenhao Lu <ctp_314@hotmail.com>
2025-12-09 22:27:46 +0800

    feat: Add trajectory recording and video logging capabilities

--- src/vlarl_infra/cli.py
+++ src/vlarl_infra/cli.py
@@ -7,17 +7,31 @@ import tyro
 from loguru import logger
 import multiprocessing 
 import gc
+import datetime
+import dateutil
+import uuid
+import pathlib
 
 import vlarl_infra.envs
+import functools
 from vlarl_infra.utils.registration import REGISTERED_ENV_CONFIGS
 from vlarl_infra.envs.base_env import BaseEnvConfig
+from vlarl_infra.utils.trajectory import Recorder
 from vlarl_client.websocket_worker_agent import WebSocketWorkerAgent
 import vlarl_infra.utils.wrappers as _wrappers
 
 _env_lock = multiprocessing.Lock()
 
 @dataclasses.dataclass
+class RecordArgs:
+    traj: bool = True
+    traj_interval: int = 10
+    video: bool = True
+    video_interval: int = 10
+
+@dataclasses.dataclass
 class Args:
+    record: RecordArgs
     uid: tyro.conf._markers.Suppress[str]
     env: BaseEnvConfig
     num_episodes: int = 1
@@ -33,6 +47,11 @@ class Args:
     viewer_host: str = "0.0.0.0"
     viewer_port: int = 9000
     
+    prefix: str | None = None
+    suffix: str | None = None
+    exp_name: str | None = None
+    log_base_dir: pathlib.Path = pathlib.Path("./logs")
+    
     use_real_time: bool = False
     fps: float = 30.0
     
@@ -41,11 +60,36 @@ class Args:
     
     pass_worker_id: bool = False
     use_env_lock: bool = False
+    
+    def __post_init__(self):
+        if self.exp_name is None:
+            if self.prefix is None:
+                self.prefix = str(uuid.uuid4().fields[-1])[:5]
 
-_CONFIGS_DICT = {k.lower(): Args(uid=k, env=v) for k, v in REGISTERED_ENV_CONFIGS.items()}
+            self.exp_name = create_exp_name(self.prefix)
+            if self.suffix is not None:
+                self.exp_name = f"{self.exp_name}_{self.suffix}"
+                
+    @property
+    def log_dir(self):
+        assert self.exp_name is not None
+        return self.log_base_dir / self.exp_name
+        
+def create_exp_name(exp_prefix, exp_id=0, seed=0):
+    """
+    Create a semi-unique experiment name that has a timestamp
+    :param exp_prefix:
+    :param exp_id:
+    :return:
+    """
+    now = datetime.datetime.now(dateutil.tz.tzlocal())
+    timestamp = now.strftime('%Y_%m_%d_%H_%M_%S')
+    return "%s_%s_%04d--s-%d" % (timestamp, exp_prefix, exp_id, seed)
+
+_CONFIGS_DICT = {k.lower(): functools.partial(Args, uid=k, env=v) for k, v in REGISTERED_ENV_CONFIGS.items()}
 
 def cli() -> Args:
-    return tyro.extras.overridable_config_cli({k: (k, v) for k, v in _CONFIGS_DICT.items()})
+    return tyro.extras.subcommand_cli_from_dict({k: v for k, v in _CONFIGS_DICT.items()})
 
 
 def _run_worker(worker_id: int, args: Args):
@@ -101,9 +145,18 @@ def _run_worker(worker_id: int, args: Args):
     if args.use_real_time:
         env = _wrappers.RealTimeWrapper(env, fps=args.fps)
         logger.info(f"Real-time mode enabled at {args.fps} FPS")
+        
+    recorder = Recorder(
+        save_dir=args.log_dir / f"worker_{worker_id}",
+        record_trajectory=args.record.traj,
+        trajectory_save_interval=args.record.traj_interval,
+        record_video=args.record.video,
+        video_record_interval=args.record.video_interval,
+    )
 
     for ep in range(args.num_episodes):
         obs, info = env.reset()
+        recorder.record_frame(ep, obs)
         action_plan = collections.deque()
         
         reward, terminated, truncated = 0.0, False, False
@@ -122,6 +175,7 @@ def _run_worker(worker_id: int, args: Args):
             
             action = action_plan.popleft()
             obs, reward, terminated, truncated, info = env.step(action)
+            recorder.record_frame(ep, obs)
             step_count += 1
             total_reward += float(reward)        
             sum_reward += float(reward)
@@ -129,10 +183,15 @@ def _run_worker(worker_id: int, args: Args):
             if not action_plan or terminated or truncated:
                 worker_agent.feedback(dataclasses.asdict(obs), float(sum_reward), terminated, truncated, info)
 
+        success = False
+        if "episode" in info and isinstance(info["episode"], dict):
+            success = info["episode"].get("s", False)
+        recorder.finish_episode(ep, total_reward, success, step_count, force=ep == args.num_episodes - 1)
         logger.debug(f"Episode {ep} finished after {step_count} steps with total reward {total_reward} and info {info}")
     env.close()
 
 def _main(args: Args):
+    multiprocessing.set_start_method("spawn", force=True)
     logger.configure(handlers=[{"sink": sys.stdout, "level": args.log_level.upper()}])
 
     logger.info(f"vlarl_infra version: {vlarl_infra.__version__}")
---
+++ src/vlarl_infra/utils/trajectory.py
@@ -0,0 +1,44 @@
+import imageio
+import pandas as pd
+import pathlib
+from vlarl_infra.envs.base_env import Observation
+
+class Recorder:
+    def __init__(self, save_dir: pathlib.Path, record_trajectory: bool = True, record_video: bool = True,
+                 trajectory_save_interval: int = 10, video_record_interval: int = 10):
+        self.save_dir = save_dir
+        if not self.save_dir.exists():
+            self.save_dir.mkdir(parents=True, exist_ok=True)
+        self.record_trajectory = record_trajectory
+        self.record_video = record_video
+        self.trajectory_save_interval = trajectory_save_interval
+        self.video_record_interval = video_record_interval
+        self.trajectory_df = pd.DataFrame(columns=["episode", "reward", "success", "length"])
+        self.writers = None
+
+    def init_writer(self, episode: int, obs: Observation):
+        if not self.record_video:
+            return
+        self.writers = {}
+        for cam_name, img_array in obs.images.items():
+            video_path = self.save_dir / f"episode_{episode:06d}_{cam_name}.mp4"
+            self.writers[cam_name] = imageio.get_writer(video_path, fps=30)
+            
+    def record_frame(self, episode: int, obs: Observation, force: bool = False):
+        if not self.record_video or (episode % self.video_record_interval != 0 and not force):
+            return
+        if self.writers is None:
+            self.init_writer(episode, obs)
+        if self.writers is not None:
+            for cam_name, img_array in obs.images.items():
+                self.writers[cam_name].append_data(img_array[0])  # Assuming batch size of 1
+            
+    def finish_episode(self, episode: int, reward: float, success: bool, length: int, force: bool = False):
+        if self.record_trajectory:
+            self.trajectory_df.loc[len(self.trajectory_df)] = [episode, reward, success, length]
+        if self.record_video and self.writers is not None:
+            for writer in self.writers.values():
+                writer.close()
+            self.writers = None
+        if episode % self.trajectory_save_interval == 0 or force:
+            self.trajectory_df.to_csv(self.save_dir / "trajectory.csv", index=False)
